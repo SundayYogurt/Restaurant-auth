2c3e27adf70edb79acec3f3cede97e6d4747ddc4
Krit <kritsanalalu5@gmail.com>
2025-06-30 17:25:39 +0700

    part 1

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("Reataurant Restful API")
+const express = require('express')
+const dotenv  = require('dotenv')
+const app = express()
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())//แปลง text เป็น json
+app.use(express.urlencoded({ extended: true})) //ให้ express มองเป็น json
+
+app.get('/', (req, res) => { // ('/') คือ path
+  res.send('Restful API')
+})
+
+app.listen(PORT,() => {
+  console.log("Listening to http://localhost:" + PORT)
+});
